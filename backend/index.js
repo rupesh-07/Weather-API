73c4f46d4efe8ad04babe5539ca0205901dc4359
rupesh-07 <rsurve214@gmail.com>
2024-10-23 15:19:29 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Home Page Backend");
+});
+
+app.get("/weather", async (req, res) => {
+  const city = req.query.city;
+  const apiKey = process.env.OPENWEATHER_API_KEY;
+
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching weather data" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
+
+// http://localhost:5000/weather?city=mumbai  --  Testing in Chrome
---
+++ frontend/src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./app.css";
+import axios from "axios";
+
+function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchWeather = async () => {
+    // Clear previous weather data and error
+    setWeather(null);
+    setError(null);
+
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/weather?city=${city}`
+      );
+
+      // console.log(response.data);
+
+      if (response.data && response.data.main) {
+        setWeather(response.data);
+      } else {
+        throw new Error("Invalid data structure");
+      }
+    } catch (err) {
+      // console.error(err);
+      setError("City not found");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value.toUpperCase())}
+        placeholder="Enter city name"
+      />
+      <button onClick={fetchWeather}>Get Weather</button>
+
+      {error && <p className="error-msg">{error}</p>}
+
+      {weather && (
+        <div className="weather-info">
+          <h2>{weather.name}</h2>
+          <p>Temperature: {weather.main.temp} °C</p>
+          <p>Weather: {weather.weather[0].description}</p>
+          <p>Humidity: {weather.main.humidity} %</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
